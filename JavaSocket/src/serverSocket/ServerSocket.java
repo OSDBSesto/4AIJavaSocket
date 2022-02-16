247d39663d395bb045f475a2dcb896e4347a2456
PoglianiMattia <mattia.pogliani@gmail.com>
2022-02-16 09:13:27 +0100

    add client socket and rename server

--- JavaSocket/src/serverSocket/ServerSocket.java
+++ JavaSocket/src/serverSocket/ServerSocket.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javasocket;
+package serverSocket;
 
 /**
  *
  * @author pogliani.mattia
  */
-public class JavaSocket {
+public class ServerSocket {
 
     /**
      * @param args the command line arguments
